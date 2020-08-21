86b101127d2bff320e91278e7a4736381033fd23
Jeremy Zucker <Jeremy.Zucker@pnnl.gov>
2020-08-20 17:30:36 -0700

    Updated setuptools for 2020

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='CausalKinetiX',
     version='0.0.1',
     description='Python implementation of CausalKinetiX (originally developed in R).',
-    long_description=readme,
+    #long_description=readme,
     author='Niklas Pfister, Stefan Bauer, Jonas Peters, Kei Ishikawa',
     author_email='niklas.pfister@stat.math.ethz.ch, stefan.bauer@inf.ethz.ch, jonas.peters@math.ku.dk, ishikawa-kei521@g.ecc.u-tokyo.ac.jp',
     url='https://github.com/NiklasPfister/CausalKinetiX-python',
-    install_requires=['numpy','scipy','quadprog','matplotlib', 'copy','glmnet','itertools']
+    install_requires=['numpy','scipy','quadprog','matplotlib', 'copy','glmnet','itertools'],
     #license=license,
-    #packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs'))
 )
